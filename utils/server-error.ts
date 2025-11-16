9e103a36b8e1a69cacc6d0056bd068a2d2c382c7
Vaibhav Jaiswar <vaibhavjaiswar98@gmail.com>
2025-11-16 16:35:43 +0530

    New server error class

---
+++ utils/server-error.ts
@@ -0,0 +1,18 @@
+"use server";
+
+export class ServerError extends Error {
+  public statusCode: number;
+  // public isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    // this.isOperational = true; // marks errors we expect (not bugs)
+
+    // Fix prototype chain (important for TS)
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    // cleaner error stack trace (removes constructor)
+    Error.captureStackTrace(this);
+  }
+}
