65f4df163edcc95f9a7827154099bf012c4be976
Vaibhav Jaiswar <58019360+vaibhavjaiswar@users.noreply.github.com>
2025-12-09 15:28:12 +0530

    Update nav-bar.tsx added z index

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -3,7 +3,7 @@ import NavBarMenu from "@/components/nav-bar-menu";
 
 export default function NavBar() {
   return (
-    <header className="sticky top-0 shadow-lg">
+    <header className="sticky top-0 shadow-lg z-40">
       <nav className="side-px  py-3 bg-slate-800 flex justify-between items-center gap-4">
         <Link
           href="/dashboard"
