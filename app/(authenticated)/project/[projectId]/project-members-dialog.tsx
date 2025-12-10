30ee4bf7aea5e28804804c16ac425850e7afe6c1
Vaibhav Jaiswar <58019360+vaibhavjaiswar@users.noreply.github.com>
2025-12-10 16:12:46 +0530

    Update project-members-dialog.tsx select member UI

--- app/(authenticated)/project/[projectId]/project-members-dialog.tsx
+++ app/(authenticated)/project/[projectId]/project-members-dialog.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useToast } from "@/context/toast-context";
 // import { addMemberToProject } from "@/apis";
 import { ProjectWithDetails } from "@/types/api-response";
+import { emailRegex } from "@/utils";
 import Dialog from "@/components/dialog";
 import LoadingButton from "@/components/loading-button";
 
@@ -16,7 +17,8 @@ interface Props {
 
 export default function ProjectMemberDialog({ open, project, setOpen }: Props) {
   const [isAddingMember, setIsAddingMember] = useState(false);
-  const [emails, setEmails] = useState<string>("");
+  const [newMemberEmailsInput, setNewMemberEmailsInput] = useState<string>("");
+  const [newMemberEmails, setNewMemberEmails] = useState<string[]>([]);
 
   const { showToast } = useToast();
   const router = useRouter();
@@ -32,7 +34,7 @@ export default function ProjectMemberDialog({ open, project, setOpen }: Props) {
       // }
 
       showToast({
-        type: "info",
+        type: "success",
         message: "Member(s) added to the project.",
       });
       setOpen(false);
@@ -53,6 +55,23 @@ export default function ProjectMemberDialog({ open, project, setOpen }: Props) {
     }
   };
 
+  useEffect(() => {
+    setNewMemberEmails([]);
+    if (newMemberEmailsInput) {
+      const emailArray = newMemberEmailsInput.split(",");
+      console.log(emailArray);
+      emailArray.forEach((email) => {
+        const isEmailValid = emailRegex.test(email);
+        console.log(
+          `Email "${email}" is ${isEmailValid ? "VALID" : "INVALID"}`
+        );
+        if (isEmailValid) {
+          setNewMemberEmails((emails) => [...emails, email]);
+        }
+      });
+    }
+  }, [newMemberEmailsInput]);
+
   return (
     <Dialog
       open={open}
@@ -72,7 +91,7 @@ export default function ProjectMemberDialog({ open, project, setOpen }: Props) {
             project.members.map((member) => (
               <span
                 key={member.user.email}
-                className="inline-block px-3 py-1 text-slate-600 hover:text-slate-800 bg-slate-100 border border-slate-200 hover:border-slate-400 rounded-full"
+                className="inline-block px-3 py-1 text-slate-600 hover:text-slate-700 bg-slate-100 border border-slate-200 hover:border-slate-300 rounded-full"
               >
                 {member.user.email}
               </span>
@@ -83,18 +102,29 @@ export default function ProjectMemberDialog({ open, project, setOpen }: Props) {
         </p>
       </div>
       <div>
-        <h2 className="mb-2 text-sm">User emails:</h2>
+        <h2 className="mb-2 text-sm">Add new members:</h2>
         <input
           type="text"
-          value={emails}
-          onChange={(e) => setEmails(e.target.value)}
-          placeholder="Enter member emails..."
+          value={newMemberEmailsInput}
+          onChange={(e) => setNewMemberEmailsInput(e.target.value)}
+          placeholder="Enter new member emails with comma..."
           className="w-full mb-4"
         />
+        {newMemberEmails.length > 0 ? (
+          <div className="mb-4 text-sm">
+            <h3 className="mb-2 text-sm">These members will be added:</h3>
+            {newMemberEmails.map((newMemberEmail) => (
+              <span className="inline-block px-3 py-1 text-green-600 hover:text-green-700 bg-green-100 border border-green-200 hover:border-green-300 rounded-full">
+                {newMemberEmail}
+              </span>
+            ))}
+          </div>
+        ) : null}
         <div className="max-w-full flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-4">
           <LoadingButton
             type="submit"
             isLoading={isAddingMember}
+            disabled={newMemberEmails.length < 1}
             onClick={handleAddMember}
             className="flex-1 primary-button w-full sm:w-auto"
           >
